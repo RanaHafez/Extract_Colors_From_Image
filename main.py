11376df09c76d7d98057491b3bfb059d55d2b208
RanaHafez <ra2017ha1999@gmail.com>
2022-07-27 13:15:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import extcolors
+from flask import Flask, jsonify, render_template, request, redirect, url_for, send_from_directory
+from tkinter import filedialog as fd
+from PIL import Image
+import base64
+import io
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "/static/images/"
+
+
+@app.route('/get-file')
+def open_file():
+    file_name = fd.askopenfilename(
+        title="Choose A Message",
+        initialdir='/',
+        filetypes=[("Image File", '.jpg'), ("Image File", '.png')],
+    )
+    if file_name:
+        print(file_name[file_name.rfind('/') + 1::])
+        colors = open_image(file_name)
+        return render_template('index.html', colors=colors, image=file_name)
+    return redirect(url_for('home'))
+
+
+def rgb_to_hex(rgb):
+    return '%02x%02x%02x' % rgb
+
+
+def open_image(img_url):
+    # img_url = open_file()
+    color_x = extcolors.extract_from_path(img_url, tolerance=12, limit=10)
+    colors = []
+    print(color_x[0])
+    for i in range(len(color_x[0])):
+        colors.append(f"#{rgb_to_hex(color_x[0][i][0])}")
+
+    return colors
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
